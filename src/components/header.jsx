01e64307d789757bed8d70a8bbfd91ce7eb6a3b9
arsscot12 <ariscot12@gmail.com>
2015-12-13 17:02:54 +0700

    updated component to render topic list in header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,26 @@
 var React = require('react');
 var Router = require('react-router');
 var Link = Router.Link;
+var Actions = require('../actions');
+var TopicStore = require('../stores/topic-stores');
+var Reflux = require('reflux');
 
 
 module.exports = React.createClass({
+	mixins: [
+		// listens for any events that are coming from TopicStore, 
+		// call onChange function when event triggered by store
+		Reflux.listenTo(TopicStore, 'onChange')
+	],
+	componentWillMount: function() {
+		// method is called from actions object in actions.jsx
+		Actions.getTopics();
+	},
+	getInitialState: function() {
+		return {
+			topics: []
+		}
+	},
 	render: function() {
 		return (
 			<nav className = "navbar navbar-default header">
@@ -12,11 +29,25 @@ module.exports = React.createClass({
 						Imgur Browser
 					</Link>
 					<ul className = "nav navbar-nav navbar-right">
-						<li><a> topic number 1</a></li>
+						{this.renderTopics()}
 					</ul>
 				</div>
 
 			</nav>
 		);
+	},
+	onChange: function(event, topics) {
+		this.setState({
+			topics: topics
+		})
+	},
+	renderTopics: function() {
+		return this.state.topics.slice(0,4).map(function(topic) {
+			return <li key = {topic.id} >
+				<Link to = {'topics/' + topic.id}>
+					{topic.name}
+				</Link>
+			</li>
+		});
 	}
 });
