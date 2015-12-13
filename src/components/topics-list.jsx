bcbd4859017b6dcf0ab1d19a53fea907eb3f7830
arsscot12 <ariscot12@gmail.com>
2015-12-13 08:36:27 +0700

    created topic-list component for rendering topics from imgur-app, uses refulx library to setState when data returned from API

---
+++ src/components/topics-list.jsx
@@ -0,0 +1,35 @@
+var React = require('react');
+var TopicStore = require('../stores/topic-stores');
+var Reflux = require('reflux');
+
+module.exports = React.createClass({
+	mixins: [
+		Reflux.listenTo(TopicStore, 'onChange')
+	],
+	getInitialState: function() {
+		return {
+			topics: []
+		}
+	},
+	componentWillMount: function() {
+		TopicStore.getTopics()
+	},
+	render: function() {
+		return <div className = "list-group">
+			Topic List
+			{this.renderTopics()}
+		</div>
+	},
+	renderTopics: function() {
+		return this.state.topics.map(function(topic) {
+			return <li>
+				{topic.name}
+			</li>
+		});
+	},
+	onChange: function(event, topicss) {
+		this.setState({
+			topics: topicss
+		})
+	}
+});
