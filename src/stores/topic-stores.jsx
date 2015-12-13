c16ca0d14c8bf99ff4566e13eed1b9530f9773c2
arsscot12 <ariscot12@gmail.com>
2015-12-13 09:03:21 +0700

    added listenables to call methods in store

--- src/stores/topic-stores.jsx
+++ src/stores/topic-stores.jsx
@@ -1,8 +1,12 @@
 var Api = require('../utils/api');
 var Reflux = require('reflux');
+var Actions = require('../actions');
 
 module.exports = Reflux.createStore({
+	// listen to any available actions provided by actions.jsx, if action matches method call it
+	listenables: [Actions],
 	getTopics: function() {
+		// fires ajax request using api module (api.jsx)
 		return Api.get('topics/defaults')
 			.then(function(json) {
 				this.topics = json.data;
@@ -11,6 +15,7 @@ module.exports = Reflux.createStore({
 		
 	},
 	triggerChange: function() {
+		// fire trigger event
 		this.trigger('change', this.topics);
 	}
 });
