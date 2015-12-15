cf207cc0a99ebb86af66788c6c59e8880f5563bf
arsscot12 <ariscot12@gmail.com>
2015-12-15 14:41:26 +0700

    created sass files for styling components

--- src/components/topic.jsx
+++ src/components/topic.jsx
@@ -1,10 +1,36 @@
 var React = require('react');
-
+var Reflux = require('reflux');
+var Actions = require('../actions');
+var ImageStore = require('../stores/image-stores');
 
 module.exports = React.createClass({
+	mixins: [
+		// listens for any events that are coming from TopicStore, 
+		// call onChange function when event triggered by store
+		Reflux.listenTo(ImageStore, 'onChange')
+	],
+	getInitialState: function() {
+		return {
+			images: []
+		}
+	},
+	componentWillMount: function() {
+		Actions.getImages(this.props.params.id);
+	},
+	componentWillReceiveProps: function(nextProps) {
+
+		Actions.getImages(nextProps.params.id);
+	},
 	render: function() {
 		return (
-			<div className="class">I'm a topic</div>
+			<div className="class">
+
+			</div>
 		);
+	},
+	onChange: function(event, images) {
+		this.setState({
+			images: images
+		})
 	}
 });
