9f7a40dc9da8e4353b7f54c579222f32de32d053
arsscot12 <ariscot12@gmail.com>
2015-12-19 08:17:44 +0700

    created component for rendering content details for single image/video

--- src/actions.jsx
+++ src/actions.jsx
@@ -3,5 +3,6 @@ var Reflux = require('reflux');
 module.exports = Reflux.createActions([
 	// single action getTopics, runs getTopics in any store that has getTopics method
 	'getTopics',
-	'getImages'
+	'getImages',
+	'getImage'
 ]);
