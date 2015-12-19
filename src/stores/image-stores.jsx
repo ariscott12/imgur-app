469656f0e57c3a275350161fedb8882c15a29636
arsscot12 <ariscot12@gmail.com>
2015-12-19 08:19:18 +0700

    added find method and getImage method for fetching single image from imgur API

--- src/stores/image-stores.jsx
+++ src/stores/image-stores.jsx
@@ -20,6 +20,32 @@ module.exports = Reflux.createStore({
 			}.bind(this));
 		
 	},
+	getImage: function(id) {
+		return Api.get('gallery/image/' + id)
+			.then(function(json) {
+				if(this.images) {
+					this.images.push(json.data);
+				} else {
+					this.images = [json.data];
+				}
+				console.log(json.data);
+				this.triggerChange();	
+			}.bind(this));
+	},
+	find: function(id) {
+		// find image with key id of propery id that is passed in
+		var image = _.find(this.images, {id: id});
+
+		// if image exists return image otherwise execute another ajax request for data
+		if(image) {
+			return image
+		} else {
+			this.getImage(id);
+			return null;
+		}
+
+		console.log(image);
+	},	
 	triggerChange: function() {
 		// fire trigger event
 		this.trigger('change', this.images);
